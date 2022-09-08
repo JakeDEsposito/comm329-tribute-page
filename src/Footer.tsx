32a567a0fcb67291b1385b1524270cefb5e48637
desposjd <94485750+desposjd@users.noreply.github.com>
2022-09-08 12:49:35 -0400

    Removed React from project

--- src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer () {
-    return (
-        <>
-            <footer className="p-3 text-center align-middle text-white">
-                Ⓒ 2022 Jake D'Esposito
-            </footer>
-        </>
-    )
-}
